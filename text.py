3269236830cf6fea46c32e620d908dafd3a6715e
Senkii <contact@senkii.space>
2021-03-23 12:00:02 +0100

    Initial commit
    Initial upload of
    - callbacks.py
    - commands.py
    - main.py
    - text.py
    Other files are still necessary for the code to work (config.json, printerdb.json), but those will not be included on github.

---
+++ text.py
@@ -0,0 +1,26 @@
+start_text = (
+'''I have sent a message to the robotic overlords with a request for print access. 
+I'll let you know when you have been accepted, please be patient!''')
+
+welcome_text = (
+'''You have been granted print access. Use /help if you need more information'''
+)
+
+help_text = (
+'''Hey, I'm Faxii, I print messages on a thermal printer on senk's desk.
+I can currently understand text, images, and stickers, and URLs will print a QR code with them, because Senk is lazy...
+For feature ideas, feel free to print a message! 
+
+Commands:
+/start  -  Show "start" message
+/help   -  Show this menu
+/info   -  Shows unformatted info about this chat'''
+)
+
+printing_text = (
+'''Printing...'''
+)
+
+no_print_access = (
+    '''Sorry, but you don't seem to have print access... You can use /start to request it!'''
+)
